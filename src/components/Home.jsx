23415a856033a8a7af2e74c0650aba8a75d27774
XAER <colonnelli.st@gmail.com>
2021-04-11 12:21:24 +0200

    modified a bit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,44 +1,35 @@
-import React from 'react'
-import { Container, Carousel, Row, Col } from 'react-bootstrap'
-import { Helmet } from 'react-helmet';
-import brain from '../assets/images/brain.png';
-import opinion from '../assets/images/opinion.png';
-import education from '../assets/images/education.png';
+import React from "react";
+import { Container, Carousel, Row, Col } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import brain from "../assets/images/brain.png";
+import opinion from "../assets/images/opinion.png";
+import education from "../assets/images/education.png";
 
 const Home = () => {
-    return (
-        <>
-            <Helmet>
-                <title>Home - La costituzione che vive</title>
-            </Helmet>
-            <section id="center-body" className="center-body">
-                <h1>Il nostro obiettivo</h1>
-                <Row style={{textAlign: "center", paddingTop: "10vh"}}>
-                    <Col>   
-                        <img
-                        src={brain}
-                        style={{height: "120px"}}
-                        />
-                        <h4 className="image-section">Conoscenza</h4>
-                    </Col>
-                    <Col>   
-                        <img
-                        src={opinion}
-                        style={{height: "120px"}}
-                        />
-                        <h4 className="image-section">Opinioni</h4>
-                    </Col>
-                    <Col>   
-                        <img
-                        src={education}
-                        style={{height: "120px"}}
-                        />
-                        <h4 className="image-section">Educazione</h4>
-                    </Col>
-                </Row>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <title>Home - La costituzione che vive</title>
+      </Helmet>
+      <section id="center-body" className="center-body">
+        <h1>Il nostro obiettivo</h1>
+        <Row style={{ textAlign: "center", paddingTop: "10vh" }}>
+          <Col>
+            <img alt="brain" src={brain} style={{ height: "120px" }} />
+            <h4 className="image-section">Conoscenza</h4>
+          </Col>
+          <Col>
+            <img alt="opinion" src={opinion} style={{ height: "120px" }} />
+            <h4 className="image-section">Opinioni</h4>
+          </Col>
+          <Col>
+            <img alt="education" src={education} style={{ height: "120px" }} />
+            <h4 className="image-section">Educazione</h4>
+          </Col>
+        </Row>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, IndexRoute } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import Home from './components/Home';
 import configureStore from './redux/configureStore';
 import { Provider } from 'react-redux'
 
@@ -15,9 +13,7 @@ export const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-          <React.StrictMode>
             <App />
-          </React.StrictMode>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
